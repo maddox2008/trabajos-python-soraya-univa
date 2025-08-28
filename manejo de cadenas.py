11e565b6d1c4bccdbb647616148cb09e79dddab6
maddox2008 <maddoxsbr8@gmail.com>
2025-08-27 19:14:09 -0600

    Add files via upload

---
+++ manejo de cadenas.py
@@ -0,0 +1,11 @@
+cadena = "soraya"
+print("capitalize" ,cadena.capitalize())
+cadena2 = "Sos"
+print("casefold", cadena2.casefold())
+
+menus = ["Menu ATM","Depositar","Retiar","Consultar"]
+for menu in menus :
+    print(menu.center(20, '-'))
+
+cadena3 = "Hola Mundoooooooooooooo"
+    print(cadena3.count("0",3,10))
